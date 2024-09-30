75f874040a940f3cac33f9c2a51ea62437e554fb
Drewcicle26 <162816717+Drewcicle26@users.noreply.github.com>
2024-09-29 20:24:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Keifer's Videos"];
+const statusMessages = ["🎧 Listening to Kiefer's Videos"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
